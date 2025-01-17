3789dda90b88bbf4c704a73fd5b02595d29fd806
Adam <adamsaul314@gmail.com>
2025-01-17 08:44:43 +0000

    updated operate function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let firstNumber;
 let operand;
 let secondNumber;
+let btnNum = 1
 
 function add(num1, num2){
     return +num1 + +num2;
@@ -18,12 +19,26 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-function operate (firstNumber, operand, secondNumber) {
-    add(firstNumber, secondNumber);
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1, num2);
+        default: return null;
+    }
 }
 
 // let num1 = prompt('Enter first number:');
 // let num2 = prompt('Enter second number:');
+const container = document.querySelector('.buttons')
+
+for (let i = 0; i < 16; i++) {
+    const buttons = document.createElement('button')
+    buttons.classList.add('button'+btnNum)
+    container.appendChild(buttons)
+    btnNum++
+}
 
 
 console.log(divide(num1, num2));
